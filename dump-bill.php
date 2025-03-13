d035f38f27d6e155f6ce70c42efa973829792006
typingmonk <tmonk.hsieh@tmonk.tw>
2025-03-13 13:54:33 +0800

    Add script for dumping meet data

--- dump-bill.php
+++ dump-bill.php
@@ -4,7 +4,7 @@ $term = $_SERVER['argv'][1] ?? 10;
 $limit = 100;
 $bill_after = null;
 $bill_show = new stdClass();
-$output = fopen(__DIR__ . "/data/bill-{$term}.jsonl", "w");
+$output = fopen(__DIR__ . "/data/bill-" . str_pad($term, 2, '0', STR_PAD_LEFT) . ".jsonl", "w");
 while (true) {
     if (is_null($bill_after)) {
         $url = sprintf("https://v2.ly.govapi.tw/bills?屆=%d&sort=議案編號>&output_fields=*&limit=%d", $term, $limit);
---
+++ dump-meet.php
@@ -0,0 +1,26 @@
+<?php
+
+$term = $_SERVER['argv'][1] ?? '10';
+$limit = 100;
+$page = 1;
+$meet_show = new stdClass();
+$output = fopen(__DIR__ . "/data/meet-" . str_pad($term, 2, '0', STR_PAD_LEFT) . ".jsonl", "w");
+while (true) {
+    $url = sprintf("https://v2.ly.govapi.tw/meets?屆=%d&sort=日期>&output_fields=*&limit=%d&page=%d", $term, $limit, $page);
+    error_log($url);
+    $ret = json_decode(file_get_contents($url));
+    $hit = 0;
+    foreach ($ret->meets as $meet) {
+        $meet_after = $meet->會議代碼;
+        if (property_exists($meet_show, $meet->會議代碼)) {
+            continue;
+        }
+        $meet_show->{$meet->會議代碼} = true;
+        fputs($output, json_encode($meet, JSON_UNESCAPED_UNICODE) . "\n");
+        $hit ++;
+    }
+    if (!$hit) {
+        break;
+    }
+    $page ++;
+}
