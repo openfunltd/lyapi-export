c3d93ebe84328d4b7c776a51585d71ee4e55c788
typingmonk <tmonk.hsieh@tmonk.tw>
2025-03-14 17:00:41 +0800

    Example of calling php script in shell script

---
+++ example.sh
@@ -0,0 +1,23 @@
+term=11
+year=2025
+
+#default term=11(hardcoded)
+php dump-bill.php $term
+
+#default term=11(hardcoded)
+php dump-meet.php $term
+
+#no argument needed
+php dump-law.php
+
+#default year is now year
+php dump-law_version.php $year
+
+#default year is now year
+php dump-law_content.php $year
+
+#no argument needed
+php dump-gazette.php
+
+#default term=11(hardcoded)
+php dump-gazette_agenda.php $term
